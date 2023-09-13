6f5a0b4e230963310e1e5a9e9f16f5b21d1ef69a
LosDelaro <cdelarosa25@yahoo.com>
2023-09-12 22:07:21 -0500

    website with tableau

--- website/static/js/tableau.js
+++ website/static/js/tableau.js
@@ -1,6 +1,57 @@
-function initViz() {
-    var containerDiv = document.getElementById("vizContainer"),
-    url = "https://YOUR-SERVER/views/YOUR-VISUALIZATION";
+var viz;
 
-    var viz = new tableau.Viz(containerDiv, url);
+$(document).ready(function() {
+    initializeViz();
+
+    $("#pdf").click(function() {
+        exportPDF();
+    });
+    $("#image").click(function() {
+        exportImage();
+    });
+    $("#crosstab").click(function() {
+        exportCrossTab();
+    });
+    $("#data").click(function() {
+        exportData();
+    });
+    $("#revert").click(function() {
+        revertAll();
+    });
+});
+
+function initializeViz() {
+    var placeholderDiv = document.getElementById("tableauViz");
+    var url = "https://public.tableau.com/views/homicide-draft3/per_100000locationhomicide";
+    var options = {
+        width: placeholderDiv.offsetWidth,
+        height: placeholderDiv.offsetHeight,
+        hideTabs: true,
+        hideToolbar: true,
+        onFirstInteractive: function() {
+            workbook = viz.getWorkbook();
+            activeSheet = workbook.getActiveSheet();
+        }
+    };
+    viz = new tableau.Viz(placeholderDiv, url, options);
+}
+
+function exportPDF() {
+    viz.showExportPDFDialog();
+}
+
+function exportImage() {
+    viz.showExportImageDialog();
+}
+
+function exportCrossTab() {
+    viz.showExportCrossTabDialog();
+}
+
+function exportData() {
+    viz.showExportDataDialog();
+}
+
+function revertAll() {
+    workbook.revertAllAsync();
 }
